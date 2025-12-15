5ca77aca44c5439f9fc7560a6ef6a03f43f13d3b
ammarmusaddiq <ammarmu007@gmail.com>
2025-12-15 11:51:15 +0500

    updates on 15 dec

--- components/ui/text-generate-effect.tsx
+++ components/ui/text-generate-effect.tsx
@@ -24,7 +24,7 @@ export const TextGenerateEffect = ({
         filter: filter ? "blur(0px)" : "none",
       },
       {
-        duration: duration ? duration : 1,
+        duration: duration ? duration : 1.5,
         delay: stagger(0.2),
       }
     );
